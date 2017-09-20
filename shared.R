a79a8fa51bf1295ed5f8663b272b9b36d689b06c
David Kulp <dkulp@dizz.org>
2017-09-19 20:34:49 -0400

    change default to /cache for prep.dir and fail if can't create caches
    intended for deployment to container

--- shared.R
+++ shared.R
@@ -1,10 +1,14 @@
+source("options.R")
+
 options(stringsAsFactors=FALSE) # plyr methods return data.frame
 options(bitmapType = 'cairo') # https://stackoverflow.com/a/17955000/86228
 
-prep.dir <- getOption('dropviz.prep.dir', default='cache')               # destination for all prepared data
+prep.dir <- getOption('dropviz.prep.dir', default='/cache')               # destination for all prepared data
 cache.dir <- 'www/cache'  # destination for renderCacheImage()
 suppressWarnings(dir.create(prep.dir, recursive = TRUE))
+stopifnot(file.exists(prep.dir))
 suppressWarnings(dir.create(cache.dir, recursive = TRUE))
+stopifnot(file.exists(cache.dir))
 
 # write either warning or message to console with a time stamp
 write.log <- function(..., warn=FALSE) {
