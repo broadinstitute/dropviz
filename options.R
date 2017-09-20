54f8a39807c516292f5b129fcf40c4d988f27105
David Kulp <dkulp@dizz.org>
2017-09-19 20:31:00 -0400

    options for overriding

---
+++ options.R
@@ -0,0 +1,7 @@
+# override defaults for development
+#
+# options(dropviz.debug=TRUE)
+# options(dropviz.prep.dir='staged') # where to write prep'd data. Move this to the live location when done
+# options(dropviz.prep.cache=FALSE)  # use existing files. Usually set this to FALSE during a prep to force since there are interdependencies that affect files such as the factor values
+# options(dropviz.experiments='exp_sets.full.txt') # only used by prep-global.R
+# options(log.reactive=TRUE) # produce detailed trace of reactives
