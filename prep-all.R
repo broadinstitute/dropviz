e1acdbcaee07b8b6a779df3e38eb1939c4b727df
David Kulp <dkulp@dizz.org>
2017-10-14 16:36:28 -0400

    Remove the check on dropviz.prep.dir

--- prep-all.R
+++ prep-all.R
@@ -2,9 +2,6 @@
 
 # adjust options.R, if needed
 
-# if prep dir is present, then an old run probably exists that
-# should be removed first
-stopifnot(!file.exists(getOption('dropviz.prep.dir')))
 
 files <- c('tSNE','metacells','components','marker','expr')
 lapply(files, function(fn) {
