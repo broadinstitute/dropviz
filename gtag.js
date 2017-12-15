944cc825ae996647b79a380b53e8fc3a8dd79b4d
David Kulp <dkulp@dizz.org>
2017-12-15 14:37:14 -0500

    Added Google Analytics

---
+++ gtag.js
@@ -0,0 +1,5 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-111320548-1');
