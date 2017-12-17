4ff77d08cb04cd9f0347da6278dc9274e21baa99
David Kulp <dkulp@dizz.org>
2017-12-17 08:43:38 -0500

    Javascript index from 0

--- gtag.js
+++ gtag.js
@@ -6,7 +6,7 @@
 
 
 $(document).on('shiny:inputchanged', function(event) {
-    if (event.name.substring(1,1)=='.') {
+    if (event.name.substring(0,1)!='.') {
 	gtag('event', 'search', { 'event_category': event.name, 'event_label': event.value } );
     }
 });
